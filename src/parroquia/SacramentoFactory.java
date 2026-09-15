cda86a538a160a0c2f59fec024174ad5bd3c5fce
proyectoparroquiajava-create <proyectoparroquia.java@gmail.com>
2026-09-15 08:58:11 -0500

    Agrega fabrica de sacramentos

---
+++ src/parroquia/SacramentoFactory.java
@@ -0,0 +1,62 @@
+package parroquia;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+
+public final class SacramentoFactory {
+
+    private static final Set<String> TIPOS_SOPORTADOS =
+            Set.of("bautizo", "matrimonio", "retiro");
+
+    private SacramentoFactory() {
+      
+    }
+
+    public static Sacramento crear(String tipo, Object... args) {
+
+        String tipoNormalizado =
+                tipo == null ? "" : tipo.trim().toLowerCase();
+
+        switch (tipoNormalizado) {
+
+            case "bautizo":
+                return new Bautizo(
+                        (Persona) args[0],
+                        (Sacerdote) args[1],
+                        (LocalDate) args[2],
+                        (Double) args[3],
+                        (Persona) args[4],
+                        (Persona) args[5]);
+
+            case "matrimonio":
+
+                @SuppressWarnings("unchecked")
+                List<Persona> testigos =
+                        (List<Persona>) args[5];
+
+                return new Matrimonio(
+                        (Feligres) args[0],
+                        (Sacerdote) args[1],
+                        (LocalDate) args[2],
+                        (Double) args[3],
+                        (Feligres) args[4],
+                        testigos);
+
+            case "retiro":
+                return new Retiro(
+                        (Feligres) args[0],
+                        (Sacerdote) args[1],
+                        (LocalDate) args[2],
+                        (Double) args[3],
+                        (Integer) args[4]);
+
+            default:
+                throw new TipoSacramentoNoSoportadoException(
+                        "El tipo '" + tipo
+                        + "' no esta soportado. Use uno de: "
+                        + TIPOS_SOPORTADOS);
+        }
+    }
+}
---
+++ src/parroquia/TipoSacramentoNoSoportadoException.java
@@ -0,0 +1,8 @@
+package parroquia;
+
+
+public class TipoSacramentoNoSoportadoException extends ExcepcionSistemaParroquial {
+    public TipoSacramentoNoSoportadoException(String mensaje) {
+        super(mensaje);
+    }
+}
